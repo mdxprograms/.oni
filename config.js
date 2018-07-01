ace6e55731ad37cd8bd18d2b03b0c405ac081d7a
Josh Waller <mdx.programs@gmail.com>
2018-06-30 22:21:52 -0400

    init oni configs

---
+++ config.js
@@ -0,0 +1,43 @@
+"use strict";
+exports.__esModule = true;
+exports.activate = function (oni) {
+    console.log("config activated");
+    // Input
+    //
+    // Add input bindings here:
+    //
+    oni.input.bind("<c-enter>", function () { return console.log("Control+Enter was pressed"); });
+    //
+    // Or remove the default bindings here by uncommenting the below line:
+    //
+    // oni.input.unbind("<c-p>")
+};
+exports.deactivate = function (oni) {
+    console.log("config deactivated");
+};
+exports.configuration = {
+    "autoUpdate.enabled": true,
+    "experimental.indentLines.enabled": true,
+    "learning.enabled": false,
+    "oni.bookmarks": ["~/repos/projects"],
+    "oni.hideMenu": true,
+    "oni.plugins.prettier": {
+        settings: {
+            semi: true
+        },
+        formatOnSave: true,
+        enabled: true
+    },
+    "editor.fontSize": "14px",
+    "editor.fontFamily": "Hack",
+    "editor.scrollBar.visible": false,
+    "editor.quickInfo.delay": 200,
+    // UI customizations
+    "ui.colorscheme": "onedark",
+    "ui.animations.enabled": true,
+    "ui.fontSmoothing": "auto",
+    "vim.setting.number": false,
+    "vim.setting.tabstop": 2,
+    "vim.setting.softtabstop": 2,
+    "vim.setting.shiftwidth": 2
+};
---
+++ config.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import * as Oni from "oni-api";
+
+export const activate = (oni: Oni.Plugin.Api) => {
+  console.log("config activated");
+
+  // Input
+  //
+  // Add input bindings here:
+  //
+  oni.input.bind("<c-enter>", () => console.log("Control+Enter was pressed"));
+
+  //
+  // Or remove the default bindings here by uncommenting the below line:
+  //
+  // oni.input.unbind("<c-p>")
+};
+
+export const deactivate = (oni: Oni.Plugin.Api) => {
+  console.log("config deactivated");
+};
+
+export const configuration = {
+  "autoUpdate.enabled": true,
+  "experimental.indentLines.enabled": true,
+  "learning.enabled": false,
+  "oni.bookmarks": ["~/repos/projects"],
+  "oni.hideMenu": true,
+  "oni.plugins.prettier": {
+    settings: {
+      semi: true
+    },
+    formatOnSave: true,
+    enabled: true
+  },
+  "editor.fontSize": "14px",
+  "editor.fontFamily": "Hack",
+  "editor.scrollBar.visible": false,
+  "editor.quickInfo.delay": 200,
+  // UI customizations
+  "ui.colorscheme": "onedark",
+  "ui.animations.enabled": true,
+  "ui.fontSmoothing": "auto",
+  "vim.setting.number": false,
+  "vim.setting.tabstop": 2,
+  "vim.setting.softtabstop": 2,
+  "vim.setting.shiftwidth": 2
+};
